7ad200900d4cea5a971b62fea6b39aaa9c35a1b7
Demidb <LLiker1337@gmail.com>
2024-07-28 21:31:38 +0300

    2

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 import os
 from dotenv import load_dotenv
 
+@app.route('/')
+def index() -> str:
+    return ('index.html')
+
+
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
