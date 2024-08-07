ebaf4b7e8ce749694d8582ef64a7ca01062798dc
Demidb <LLiker1337@gmail.com>
2024-08-07 11:13:42 +0300

    5

--- page_analyzer/url.py
+++ page_analyzer/url.py
@@ -1,16 +1,18 @@
 from urllib.parse import urlparse
 
-from validator_collection import validators
+import validators
 
 MAX_URL_LEN = 255
 
 
 def normalize_url(url: str) -> str:
+    """Truncates the URL to the <protocol>://<domain name> structure"""
     parsed_url = urlparse(url)
     return f'{parsed_url.scheme}://{parsed_url.netloc}'
 
 
 def validate_url(url: str) -> list[str]:
+    """Validate url by rules (https://gist.github.com/dperini/729294)."""
     errors = []
     if url == '':
         errors.append('No url')
